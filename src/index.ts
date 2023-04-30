79ea6df775815cd459ccdb2bc16cb1b58354c4f1
Dylan Rafferty <draff1800@gmail.com>
2023-04-29 21:01:30 -0400

    Working prototype on converting xml to json and saving it as a file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello world!');
+import * as xml2js from 'xml2js';
+import * as fs from 'fs';
+
+const xml = `<?xml version="1.0" encoding="UTF-8" ?>
+            <user id="1">
+                <name>John Doe</name>
+                <email>john.doe@example.com</email>
+                <roles>
+                    <role>Member</role>
+                    <role>Admin</role>
+                </roles>
+                <admin>true</admin>
+            </user>`;
+
+xml2js
+  .parseStringPromise(xml, { mergeAttrs: true })
+  .then((result) => {
+    const json = JSON.stringify(result, null, 4);
+    fs.writeFileSync('user.json', json);
+  })
+  .catch((err) => console.log(err));
