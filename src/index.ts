92786d2851f893f357fa5e1b771d194dab2edf47
Dylan Rafferty <draff1800@gmail.com>
2023-05-01 18:28:41 -0400

    Implemented scheduled task to update MongoDB records every midnight

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import schedule from 'node-schedule';
 import vehicleRoutes from './routes/vehicleRoutes';
 import { connectToDB } from './utils/database';
+import { saveAndGetMakesWithTypes } from './services/vehicleService';
 
 connectToDB()
   .then(() => {
@@ -8,6 +10,8 @@ connectToDB()
     const PORT = process.env.PORT || 3000;
 
     app.use('/vehicles', vehicleRoutes);
+
+    schedule.scheduleJob('0 0 * * *', saveAndGetMakesWithTypes);
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
